967243b750508cbdf7ca6fc02988ea96e53b765a
AntoKeinanen <anto.keinanen@gmail.com>
2025-01-25 20:45:57 +0200

    2.8, 2.9*: puhelinluettelo step3 & step4

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,35 +1,55 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  // const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [search, setSearch] = useState("");
 
   function handleAddPerson(event) {
     event.preventDefault();
     const name = event.target.name.value;
+    const number = event.target.number.value;
+
     if (persons.find((p) => p.name == name)) {
       window.alert(`${name} is already added to phonebook`);
       return;
     }
-    
-    setPersons((p) => [...p, { name }]);
+
+    setPersons((p) => [...p, { name, number }]);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        search: <input onChange={(e) => setSearch(e.target.value)} />
+      </div>
+      <h3>Add new</h3>
       <form onSubmit={handleAddPerson}>
         <div>
           name: <input id="name" />
+        </div>
+        <div>
+          number: <input id="number" />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <p key={i}>{person.name}</p>
-      ))}
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(search.toLowerCase())
+        )
+        .map((person, i) => (
+          <p key={i}>
+            {person.name} {person.number}
+          </p>
+        ))}
     </div>
   );
 };
