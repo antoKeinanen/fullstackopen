ea4883f4e0c5f25a011fe438676de21d43c33a9c
AntoKeinanen <anto.keinanen@gmail.com>
2025-01-25 16:30:08 +0200

    1.7: unicafe step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positiveRatio, setPositiveRatio] = useState(0);
+
+  useEffect(() => {
+    setAverage((good - bad) / (good + neutral + bad));
+    setPositiveRatio((good / (good + neutral + bad)) * 100);
+  }, [good, neutral, bad]);
 
   function handleReview(rating) {
     if (rating == "good") setGood((v) => v + 1);
@@ -28,6 +35,8 @@ const App = () => {
           <p>good {good}</p>
           <p>neutral {neutral}</p>
           <p>bad {bad}</p>
+          <p>average {average}</p>
+          <p>positive ratio {positiveRatio}%</p>
         </div>
       </section>
     </main>
