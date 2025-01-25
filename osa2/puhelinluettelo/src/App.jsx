9a0ac26c62a0752ed8351b2496b18642f974d70a
AntoKeinanen <anto.keinanen@gmail.com>
2025-01-25 20:31:54 +0200

    2.6: puhelinluettelo step1

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  // const [newName, setNewName] = useState("");
+
+  function handleAddPerson(event) {
+    event.preventDefault();
+    const name = event.target.name.value;
+    setPersons((p) => [...p, { name }]);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name: <input id="name" />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) => (
+        <p key={i}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
