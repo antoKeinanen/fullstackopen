f61f4bee175c333baacb82be5e6c397bd2dc24c4
AntoKeinanen <anto.keinanen@gmail.com>
2025-01-25 20:10:37 +0200

    2.2: kurssitiedot step7

--- osa2/reactin_alkeet/src/components/total.jsx
+++ osa2/reactin_alkeet/src/components/total.jsx
@@ -1,5 +1,5 @@
 function Total({ parts }) {
-  return <p>Number of exercises {parts.reduce((acc, p) => acc + p.exercises, 0)}</p>;
+  return <b>Number of exercises {parts.reduce((acc, p) => acc + p.exercises, 0)}</b>;
 }
 
 export default Total;
