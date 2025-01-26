711c86a6e8517d37fae232fd34c41d9f4a8e93ef
AntoKeinanen <anto.keinanen@gmail.com>
2025-01-26 12:23:38 +0200

    2.17*: puhelinluettelo step12

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -3,10 +3,13 @@ import Filter from "./components/filter";
 import PersonForm from "./components/person_form";
 import Persons from "./components/persons";
 import personService from "./services/persons";
+import Notification from "./components/notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [search, setSearch] = useState("");
+  const [message, setMessage] = useState(null);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     personService.getAllPersons().then((resp) => setPersons(resp.data));
@@ -26,28 +29,52 @@ const App = () => {
         `${name} is already added to phonebook would you like to update the number?`
       )
     ) {
-      personService.updatePerson(found.id, newPerson).then(console.log);
-      setPersons(persons.map((p) => (p.id == found.id ? newPerson : p)));
+      personService
+        .updatePerson(found.id, newPerson)
+        .then(() => {
+          setPersons(persons.map((p) => (p.id == found.id ? newPerson : p)));
+
+          setMessage(`Updated ${found.name}`);
+          setIsError(false);
+
+          setTimeout(() => setMessage(null), 2000);
+        })
+        .catch(() => {
+          setMessage(`${found.name} has already been deleted`);
+          setIsError(true);
+
+          setTimeout(() => setMessage(null), 2000);
+        });
+
       return;
     }
 
-    personService.createPerson(newPerson).then((resp) => {
-      console.log(resp);
+    personService.createPerson(newPerson).then(() => {
       setPersons((p) => [...p, newPerson]);
+
+      setMessage(`Added ${newPerson.name}`);
+      setIsError(false);
+
+      setTimeout(() => setMessage(null), 2000);
     });
   }
 
   function handleDeletePerson(id, name) {
     if (window.confirm(`Delete ${name}?`))
-      personService.deletePerson(id).then((resp) => {
-        console.log(resp);
+      personService.deletePerson(id).then(() => {
         setPersons(persons.filter((p) => p.id != id));
+
+        setMessage(`Deleted ${name}`);
+        setIsError(false);
+
+        setTimeout(() => setMessage(null), 2000);
       });
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} isError={isError} />
       <Filter setSearch={setSearch} />
       <h2>Add new</h2>
       <PersonForm onSubmit={handleAddPerson} />
